e992b60f3168832e6eca7c0bd5cb5062f3aad962
燦 <422032918@QQ.com>
2025-05-25 10:11:57 +0800

    feat: css练习

---
+++ src/router/modules/practiceCSS.ts
@@ -0,0 +1,9 @@
+// 最简代码，也就是这些字段必须有
+export default {
+  path: "/practiceCSS/index",
+  name: "practiceCSS",
+  component: () => import("@/views/practiceCSS/index.vue"),
+  meta: {
+    title: "CSS练习"
+  }
+};
---
+++ src/views/practiceCSS/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="width: 100%; height: 100%">
+    <!-- <div class="box">盒子</div>
+    <div class="box1">盒子下</div> -->
+
+    <div class="fixe-style">
+      <div style="background-color: red">1</div>
+      <div style="background-color: blue">1</div>
+      <div style="background-color: yellow">1</div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineOptions({
+  name: "practiceCSS"
+});
+</script>
+
+<style>
+.box {
+  width: 100px;
+  height: 100px;
+
+  /* margin: 100px; */
+  background-color: rgb(156 239 21);
+}
+
+.box1 {
+  width: 100px;
+  height: 100px;
+  background-color: rgb(67 81 28);
+
+  /* padding: 10px; */
+}
+
+.fixe-style {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 600px;
+  height: 400px;
+  background-color: beige;
+}
+
+/* fixe-style样式下的所有div的样式 */
+.fixe-style div {
+  width: 40px;
+  height: 40px;
+}
+</style>
